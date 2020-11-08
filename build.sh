84aaa3df4ab423732e63c8affbbaef6acf35a005
kare <58642627+kareotoko@users.noreply.github.com>
2020-11-09 08:25:16 +0900

    Update build.sh
    added comment in bahasa and link of explanation

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
+## Ini adalah file pertama yang dieksekusi untuk membangun ORB-SLAM3
+## Di dalamnya terdapat serangkaian langkah2 untuk membangun dependencies yang diakhiri dengan ORB-SLAM3
+
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
-cd Thirdparty/DBoW2
+cd Thirdparty/DBoW2 # Thirdparty tidak diinstall lebih dulu, ??apa alasan khususnya ya.
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
@@ -8,7 +11,8 @@ make -j
 
 cd ../../g2o
 
-echo "Configuring and building Thirdparty/g2o ..."
+echo "Configuring and building Thirdparty/g2o ..." # Thirdparty tidak diinstall lebih dulu, ??apa alasan khususnya ya.
+mkdir build
 
 mkdir build
 cd build
@@ -17,7 +21,7 @@ make -j
 
 cd ../../../
 
-echo "Uncompress vocabulary ..."
+echo "Uncompress vocabulary ..." # Vocabulary itu isinya sangat penting yaitu https://webdiis.unizar.es/~dorian/papers/GalvezTRO12.pdf | baca juga https://github.com/raulmur/ORB_SLAM2/issues/139
 
 cd Vocabulary
 tar -xf ORBvoc.txt.tar.gz
